c71347d5eb25dd9b144c154e74135813e07cacbc
Eliel <e.rantalaiho@gmail.com>
2019-09-21 12:11:20 +0300

    Rakenna spawningpool

--- botti.py
+++ botti.py
@@ -1,14 +1,21 @@
 import sc2
 from sc2 import run_game, maps, Race, Difficulty
 from sc2.player import Bot, Computer
+from sc2.units import UnitTypeId
 
-class WorkerRushBot(sc2.BotAI):
-    async def on_step(self, iteration):
-        if iteration == 0:
-            for worker in self.workers:
-                await self.do(worker.attack(self.enemy_start_locations[0]))
+class Tyoskentelijat(sc2.BotAI):
+    async def on_step(self, iteration): 
+        if iteration == 1:
+            await self.chat_send('Mursut tulevat')
+        await self.distribute_workers()
+        await self.rakenna_spawningpool()
+
+    async def rakenna_spawningpool(self):
+        if (self.can_afford(UnitTypeId.SPAWNINGPOOL) and not self.already_pending(UnitTypeId.SPAWNINGPOOL)):
+            await self.chat_send('Rakennetaan spawning pool!')
+            await self.build(UnitTypeId.SPAWNINGPOOL, self.start_location, max_distance=20)
 
 run_game(maps.get("Triton LE"), [
-    Bot(Race.Zerg, WorkerRushBot()),
+    Bot(Race.Zerg, Tyoskentelijat()),
     Computer(Race.Zerg, Difficulty.Easy)
 ], realtime=True)
